a7f8568033234e448b651d12a56f6eb857cc7334
Walker Denial <WalkerDenial@qq.com>
2022-08-19 18:01:36 +0800

    feat: 创建汇总界面

--- app/src/main/java/com/wd/cd/MainActivity.kt
+++ app/src/main/java/com/wd/cd/MainActivity.kt
@@ -1,26 +1,22 @@
 package com.wd.cd
 
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.spring
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ExpandLess
-import androidx.compose.material.icons.filled.ExpandMore
-import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
+import androidx.compose.material.Divider
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.wd.cd.ui.theme.ComposeDemoTheme
 
@@ -29,123 +25,39 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeDemoTheme {
-                MyApp()
+                ActivityList(this)
             }
         }
     }
 }
 
-@Composable
-fun MyApp() {
-    var shouldShowOnBoarding by rememberSaveable {
-        mutableStateOf(true)
-    }
-    if (shouldShowOnBoarding) {
-        OnBoardingScreen(onContinueClicked = { shouldShowOnBoarding = false })
-    } else {
-        Greetings()
-    }
-}
+data class ActivityModel<T : ComponentActivity>(val name: String, val clazz: Class<T>)
 
-@Composable
-fun OnBoardingScreen(onContinueClicked: () -> Unit) {
-    Surface {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(text = "Welcome to the Basics CodeLab!")
-            Button(modifier = Modifier.padding(vertical = 24.dp), onClick = onContinueClicked) {
-                Text(text = "Continue")
-            }
-        }
-    }
-}
-
-@Composable
-private fun Greetings(names: List<String> = List(1000) { "$it" }) {
-    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
-        items(items = names) { name ->
-            Greeting(name = name)
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Card(
-        backgroundColor = MaterialTheme.colors.primary,
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
-    ) {
-        CardContent(name)
-    }
-}
-
-@Composable
-private fun CardContent(name: String) {
-    var expanded by remember { mutableStateOf(false) }
-
-    Row(
-        modifier = Modifier
-            .padding(12.dp)
-            .animateContentSize(
-                animationSpec = spring(
-                    dampingRatio = Spring.DampingRatioMediumBouncy,
-                    stiffness = Spring.StiffnessLow
-                )
-            )
-    ) {
-        Column(
-            modifier = Modifier
-                .weight(1f)
-                .padding(12.dp)
-        ) {
-            Text(text = "Hello,")
-            Text(
-                text = name,
-                style = MaterialTheme.typography.h4.copy(fontWeight = FontWeight.ExtraBold)
-            )
-            if (expanded) {
-                Text(
-                    text = ("Composem ipsum color sit lazy, " + "padding theme elit, sed do bouncy. ").repeat(
-                        4
-                    )
-                )
-            }
-        }
-        IconButton(onClick = { expanded = !expanded }) {
-            Icon(
-                imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
-                contentDescription = if (expanded) {
-                    stringResource(R.string.show_less)
-                } else {
-                    stringResource(R.string.show_more)
-                }
-            )
-        }
-    }
-}
-
-@Preview(showBackground = true, widthDp = 320, heightDp = 320)
-@Composable
-fun OnBoardingScreenPreview() {
-    ComposeDemoTheme {
-        OnBoardingScreen(onContinueClicked = {})
-    }
-}
-
-@Preview(
-    showBackground = true,
-    widthDp = 320,
-    uiMode = UI_MODE_NIGHT_YES,
-    name = "DefaultPreviewDark"
+val activities = listOf(
+    ActivityModel("基础知识", BasicKnowledgeActivity::class.java),
+    ActivityModel("基础知识", BasicKnowledgeActivity::class.java)
 )
 
-@Preview(showBackground = true, widthDp = 320)
 @Composable
-fun DefaultPreview() {
-    ComposeDemoTheme {
-        Greetings()
+fun ActivityList(context: Context) {
+    LazyColumn(Modifier.fillMaxWidth()) {
+        items(items = activities) { item ->
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Text(
+                    text = "Jetpack Compose ${item.name}", modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable { context.startActivity(Intent(context, item.clazz)) }
+                        .padding(8.dp)
+                )
+                Divider(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(1.dp), color = Color.LightGray
+                )
+            }
+        }
     }
 }
