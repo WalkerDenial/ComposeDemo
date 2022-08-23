8095569ec066f164bbde423b16add989521635bf
Walker Denial <WalkerDenial@qq.com>
2022-08-23 19:30:14 +0800

    feat: task list dev

--- app/src/main/java/com/wd/cd/state/WellnessTaskList.kt
+++ app/src/main/java/com/wd/cd/state/WellnessTaskList.kt
@@ -1,5 +1,11 @@
 package com.wd.cd.state
 
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+
 /**
  *
  * @Author Walker Denial
@@ -7,3 +13,17 @@ package com.wd.cd.state
  * @Time Created at 2022/8/23 19:13.
  */
 private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
+
+@Composable
+fun WellnessTasksList(
+    modifier: Modifier = Modifier,
+    list: List<WellnessTask> = remember {
+        getWellnessTasks()
+    }
+) {
+    LazyColumn(modifier = modifier) {
+        items(list) { task ->
+            WellnessTaskItem(taskName = task.label)
+        }
+    }
+}
